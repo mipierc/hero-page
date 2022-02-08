cc73d9a53a1c55bb216893a08f8c27a0fb0fa5e8
mipierc <mpierce2602@gmail.com>
2022-02-08 17:40:39 -0500

    implements hero page with tailwind

---
+++ public/app.js
@@ -0,0 +1,31 @@
+const bars = document.querySelector('#bars')
+const menu = document.querySelector('#menu')
+
+bars.addEventListener('click', () => {
+    if (menu.classList.contains('hidden') && window.innerWidth < 768) {
+        menu.classList.remove('hidden');
+        menu.classList.add(
+            'flex',
+            'flex-col',
+            'text-center',
+            'w-full',
+            'absolute',
+            'top-16'
+        );
+    } else {
+        menu.classList.add('hidden');
+    }
+});
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth >768) {
+        menu.classList.remove(
+            'flex',
+            'flex-col',
+            'text-center',
+            'w-full',
+            'absolute',
+            'top-16'
+        );
+    }
+})
